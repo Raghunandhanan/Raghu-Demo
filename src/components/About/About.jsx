2ca168c7bafac1a9c396d567783ebf09f777b8b6
Raghunandhanan Narasimhan <Raghunandhanan.N@ZIFOCORP.COM>
2021-01-20 20:43:22 +0530

    Test

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -39,14 +39,14 @@ const About = () => {
               <div className="about-wrapper__info">
                 <p className="about-wrapper__info-text">
                   {paragraphOne ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                    'This is a sample POC to explore the functionalities of Gatsby'}
                 </p>
                 <p className="about-wrapper__info-text">
                   {paragraphTwo ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                    'The next steo would be to explore how this can be integrated with a Git repo and also with Netlify to continously this Gtasby dummy site.'}
                 </p>
                 <p className="about-wrapper__info-text">
-                  {paragraphThree || 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'}
+                  {paragraphThree || 'All this text has been edited with the code to check whether it gets reflected properly or not. yay!'}
                 </p>
                 {resume && (
                   <span className="d-flex mt-3">
